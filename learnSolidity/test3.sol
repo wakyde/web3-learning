97f088cc7ec1c43c5549fdc0e29aadd05f233821
wakyde <wakyde@163.com>
2025-10-27 17:25:39 +0800

    本地临时存储

---
+++ learnSolidity/test3.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+contract Test3{
+    function fCalldata(uint[] calldata _x) public pure returns(uint[] calldata){
+        //参数为calldata数组，不能被修改
+        // _x[0] = 0 //这样修改会报错
+        return(_x);
+    }
+    uint[] x = [1,2,3]; // 状态变量：数组 x
+
+    function fStorage() public{
+        //声明一个storage的变量 xStorage，指向x。修改xStorage也会影响x
+        uint[] storage xStorage = x;
+        xStorage[0] = 100;
+    }
+    function fMemory() public view {
+        uint[] memory xMemory = x;
+        xMemory[0] = 200;
+    }
+}
